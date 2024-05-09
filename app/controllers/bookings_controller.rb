015b38810853589b4af0abfc138f63e1936cd6cd
prathikshark <96731595+prathikshark@users.noreply.github.com>
2024-05-09 22:05:33 +0530

    booking cjanged

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -20,7 +20,6 @@ class BookingsController < ApplicationController
        current_booking_id = params[:id]
        current_booking = Booking.find_by(id: current_booking_id)
        current_booking.update(booked: true)
-       confirm(cart)
        create
        redirect_to  booking_confirmed_bookings_path
     end
@@ -29,33 +28,12 @@ class BookingsController < ApplicationController
     
     end
     
-    # def destroy
-    #     cart = current_user.customer.cart
-    #     booking = Booking.create(customer_id: current_user.customer.id)
-      
-    #     cart.cart_services.each do |cart_service|
-    #         from_date = cart_service.from_date
-    #         to_date = cart_service.to_date
-    #         # Creating Unavailability  for each date in the range
-    #         (from_date..to_date).each do |date|
-    #             Unavailability.create(worker_id: cart_service.worker_id, unavailable_date: date)
-    #         end
-    #         # Creating BookedService for date range
-    #         booking.booked_services.create(cart_service.attributes.slice('worker_id', 'from_date', 'to_date', 'skill_type', 'shift', 'time', 'hour_per_day', 'wage'))
-    #     end
-
-    #     confirm(cart)
-    #     # Destroy CartService  after the save
-    #     cart.cart_services.destroy_all
-    #     redirect_to request.referer
-    #   end
-
       #email
-      def confirm(cart)
-        cart.cart_services.each do |cart_service|
-          worker = Worker.find_by(id: cart_service.worker_id)
-          WorkerMailer.confirmation_email(worker).deliver_now if worker
-        end
-      end    
+    def confirm(cart)
+      cart.cart_services.each do |cart_service|
+        worker = Worker.find_by(id: cart_service.worker_id)
+        WorkerMailer.confirmation_email(worker).deliver_now if worker
+      end
+    end    
  
 end
